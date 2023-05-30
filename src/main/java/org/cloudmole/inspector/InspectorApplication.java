e1f3b3007eab8da3ca80b29e12b738c7d8b8c552
Gordon Byron <gbyron@contractor.worldremit.com>
2023-05-30 13:02:02 +0200

    First commit, project builds and runs

---
+++ src/main/java/org/cloudmole/inspector/InspectorApplication.java
@@ -0,0 +1,13 @@
+package org.cloudmole.inspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectorApplication.class, args);
+	}
+
+}
